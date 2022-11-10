c7113bfef165aa7c350a5c70af79bab2dec6ad8b
mohanbendegumbali <mohan0311@hotmail.com>
2022-11-10 19:54:32 +0530

    [Mohan] Add: Address Book

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        List<Contact> address = new List<Contact>();
+        Contact contact = new Contact();
+        public void CreateContact()
+        {
+            Console.WriteLine(" Enter the first name ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine(" Enter the last name ");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine(" Enter the address ");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine(" Enter the city ");
+            contact.City = Console.ReadLine();
+            Console.WriteLine(" Enter the state ");
+            contact.State = Console.ReadLine();
+            Console.WriteLine(" Enter the Phone number ");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine(" Enter the Email Id ");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("Enter the Zip");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            address.Add(contact);
+        }
+    }
+}
